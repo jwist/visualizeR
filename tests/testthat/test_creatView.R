8e940401b0cd8b33e9653b26eb1483990184ec01
jul <jul@darmn3>
2018-06-18 07:22:07 -0500

    including datasets and documentation

--- tests/testthat/test_creatView.R
+++ tests/testthat/test_creatView.R
@@ -1,9 +1,5 @@
+
 context("testing createView")
-
-#source("../../R/readView.R")
-#source("../../R/saveJSON.R")
-#source("../../R/createView.R")
-
 
 test_that('blank view exists', {
   v <- createView()
