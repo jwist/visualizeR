ea40ac550b7e37a03c27295ff9fdcb9e59781635
Julien Wist <jwist@users.noreply.github.com>
2018-08-28 19:39:41 -0500

    improve coverage

--- tests/testthat/test_appendAndPush.R
+++ tests/testthat/test_appendAndPush.R
@@ -2,7 +2,7 @@
 context("testing append and push")
 
 
-test_that('push creates file', {
+test_that('append appends and push creates file', {
   x <- seq(from = 0, to = pi, by = 0.1)
   d <- appendData(variableName = "simpleArray", variable = cos(x), type = "1D Y array")
   d <- appendData(data = d, variableName = "simpleArray2", variable = sin(x), type = "1D Y array")
@@ -14,7 +14,12 @@ test_that('push creates file', {
   push(v, type="data", d)
 
   pathToFile <- file.path( system.file(package = "visualizeR"), "visu", "data", "data.json" )
+  expect_true(system(paste("ls", pathToFile)) == 0)
 
+  view <- list()
+  push(v, type="view", view)
+
+  pathToFile <- file.path( system.file(package = "visualizeR"), "visu", "view", "view.json" )
   expect_true(system(paste("ls", pathToFile)) == 0)
 
 })
