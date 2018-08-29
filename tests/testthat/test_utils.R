781a96438803f5a0cc3657fcc5fa423622873e17
jul <jul@darmn3>
2018-08-29 14:43:43 -0500

    include getColor2 and visualization into coverage

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,11 +1,16 @@
 
 context("testing utils")
 
-test_that('rep.row works', {
+test_that('repRow works', {
   expect_equal( nrow(repRow(c(1,2,3), 3)), 3)
 })
 
-test_that('rep.col works', {
+test_that('repCol works', {
   expect_equal( ncol(repCol(c(1,2,3), 3)), 3)
+})
+
+test_that('getColor2 works', {
+  expect_equal( getColor2("test", "rgb"), "rgba(97,64,81,1)")
+  expect_equal( getColor2("test", "hex"), "#614051")
 })
 
