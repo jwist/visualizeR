f2927786d07da1ff7ff06468de13bb1464b31a53
jul <jul@darmn3>
2019-06-28 13:34:03 -0500

    add read_data function

--- R/read_data.R
+++ R/read_data.R
@@ -1,5 +1,7 @@
 
 #' load data from csv files produced with
+#' https://www.cheminfo.org/?viewURL=https%3A%2F%2Fcouch.cheminfo.org%2Fcheminfo-public%2F0e380c7e7bb1178ca434122bca5ba0b9%2Fview.json&loadversion=true&fillsearch=Create+matrix+from+Bruker+files
+#'
 #'
 #' @param filepath the visualization to be updated
 #' @return an X matrix, a vector for x axis, a vector with expnos and a vector with expnames
